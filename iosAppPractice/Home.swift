20c87f659f4f278a53580df87dc900281da63f49
ta_fukase <>
2019-03-11 18:12:03 +0900

    QR読み込み機能の追加

--- iosAppPractice/Home.swift
+++ iosAppPractice/Home.swift
@@ -7,8 +7,11 @@
 //
 
 import UIKit
+import AVFoundation
 
-class Home: UIViewController {
+class Home: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
+    
+    private let session = AVCaptureSession()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,6 +20,69 @@ class Home: UIViewController {
     
     @IBAction func onButtonQR(_ sender: Any) {
         
+        // カメラやマイクのデバイスそのものを管理するオブジェクトを生成（ここではワイドアングルカメラ・ビデオ・背面カメラを指定）
+        let discoverySession = AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInWideAngleCamera],
+                                                                mediaType: .video,
+                                                                position: .back)
+        
+        // ワイドアングルカメラ・ビデオ・背面カメラに該当するデバイスを取得
+        let devices = discoverySession.devices
+        
+        //　該当するデバイスのうち最初に取得したものを利用する
+        if let backCamera = devices.first {
+            do {
+                // QRコードの読み取りに背面カメラの映像を利用するための設定
+                let deviceInput = try AVCaptureDeviceInput(device: backCamera)
+                
+                if self.session.canAddInput(deviceInput) {
+                    self.session.addInput(deviceInput)
+                    
+                    // 背面カメラの映像からQRコードを検出するための設定
+                    let metadataOutput = AVCaptureMetadataOutput()
+                    
+                    if self.session.canAddOutput(metadataOutput) {
+                        self.session.addOutput(metadataOutput)
+                        
+                        metadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+                        metadataOutput.metadataObjectTypes = [.qr]
+                        
+                        // 背面カメラの映像を画面に表示するためのレイヤーを生成
+                        let previewLayer = AVCaptureVideoPreviewLayer(session: self.session)
+                        previewLayer.frame = self.view.bounds
+                        previewLayer.videoGravity = .resizeAspectFill
+                        self.view.layer.addSublayer(previewLayer)
+                        
+                        // 読み取り可能エリアの設定を行う
+                        // 画面の横、縦に対して、左が10%、上が40%のところに、横幅80%、縦幅20%を読み取りエリアに設定
+                        let x: CGFloat = 0.1
+                        let y: CGFloat = 0.4
+                        let width: CGFloat = 0.8
+                        let height: CGFloat = 0.2
+                        metadataOutput.rectOfInterest = CGRect(x: y, y: 1 - x - width, width: height, height: width)
+                        
+                        // 読み取り可能エリアに赤い枠を追加する
+                        let detectionArea = UIView()
+                        detectionArea.frame = CGRect(x: view.frame.size.width * x, y: view.frame.size.height * y, width: view.frame.size.width * width, height: view.frame.size.height * height)
+                        detectionArea.layer.borderColor = UIColor.red.cgColor
+                        detectionArea.layer.borderWidth = 3
+                        view.addSubview(detectionArea)
+                        
+                        // 閉じるボタン
+                        let closeBtn:UIButton = UIButton()
+                        closeBtn.frame = CGRect(x: 50, y: 50, width: 100, height: 40)
+                        closeBtn.setTitle("閉じる", for: UIControl.State.normal)
+                        closeBtn.backgroundColor = UIColor.lightGray
+                        closeBtn.addTarget(self, action: #selector(closeTaped(sender:)), for: .touchUpInside)
+                        self.view.addSubview(closeBtn)
+                        
+                        // 読み取り開始
+                        self.session.startRunning()
+                    }
+                }
+            } catch {
+                print("Error occured while creating video device input: \(error)")
+            }
+        }
     }
     
     @IBAction func onButtonWeather(_ sender: Any) {
@@ -35,5 +101,40 @@ class Home: UIViewController {
         let nextViewController = storyboard.instantiateInitialViewController()!
         present(nextViewController, animated: true, completion: nil)
     }
-}
+    
+    func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
+        for metadata in metadataObjects as! [AVMetadataMachineReadableCodeObject] {
+            // QRコードのデータかどうかの確認
+            if metadata.type != .qr { continue }
+            
+            // QRコードの内容が空かどうかの確認
+            if metadata.stringValue == nil { continue }
+            
+            /*
+             このあたりで取得したQRコードを使ってゴニョゴニョする
+             読み取りの終了・再開のタイミングは用途によって制御が異なるので注意
+             以下はQRコードに紐づくWebサイトをSafariで開く例
+             */
+            
+            // URLかどうかの確認
+            if let url = URL(string: metadata.stringValue!) {
+                  //TODO: URLを取得した後の処理を実装する -> 現状はとりあえずweatherPageに遷移するようにしている
+//                // 読み取り終了
+//                self.session.stopRunning()
+//                // QRコードに紐付いたURLをSafariで開く
+//                UIApplication.shared.open(url, options: [:], completionHandler: nil)
+                
+                moveToNextPage(nextStoryboardName: "weatherPage")
 
+                break
+            }
+        }
+    }
+    
+    // 閉じるが押されたら呼ばれます
+    
+    @objc
+    func closeTaped(sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+    }
+}
