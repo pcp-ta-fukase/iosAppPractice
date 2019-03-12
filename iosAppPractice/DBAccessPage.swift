2977f8e93a374fa600bf26ca8bcbc4c88cdece43
ta_fukase <>
2019-03-12 18:19:08 +0900

    配列の値をtableViewに表示するように実装

--- iosAppPractice/DBAccessPage.swift
+++ iosAppPractice/DBAccessPage.swift
@@ -10,16 +10,18 @@
 import UIKit
 
 class DBAccessPage: UIViewController, UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate {
-    
+        
     @IBOutlet weak var nameTextField: UITextField!
     
     @IBOutlet weak var nameTableView: UITableView!
+    
+    var names: [String] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        nameTextField.delegate = self
+        nameTextField.delegate = self        
         nameTableView.delegate = self
     }
     
@@ -28,10 +30,17 @@ class DBAccessPage: UIViewController, UITableViewDelegate, UITableViewDataSource
         self.dismiss(animated: true, completion: nil)
     }
     
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+    @IBAction func onButtonSend(_ sender: Any) {
+
+        //現在表示されているテキストフィールドの文字列を配列に追加
+        names.append(nameTextField.text!)
         
-        textField.text = "CHANGE!!"
-            
+        nameTableView.reloadData()
+    }
+    
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+
         //Keyboardを閉じる
         textField.resignFirstResponder()
         
@@ -43,22 +52,20 @@ class DBAccessPage: UIViewController, UITableViewDelegate, UITableViewDataSource
         
         if (self.nameTextField.isFirstResponder) {
             
-            self.nameTextField.text = "YOU TOUCHED OUTSIDE!"
             self.nameTextField.resignFirstResponder()
         }
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return names.count
     }
     
-    // セルを返す
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         // セルにテキストを出力する。
-        let cell = tableView.dequeueReusableCell(withIdentifier:  "cell", for:indexPath as IndexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath as IndexPath)
         
         // セルに表示する値（ラベルの文字など）を設定する
-        cell.textLabel!.text = "あいうえお"
+        cell.textLabel!.text = names[indexPath.row]
         
         return cell
     }
