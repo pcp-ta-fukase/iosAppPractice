c3627c556c021f863891b4727864ca778530c427
ta_fukase <>
2019-03-12 11:42:57 +0900

    QRResultPageの追加

---
+++ iosAppPractice/QRResultPage.swift
@@ -0,0 +1,18 @@
+//
+//  QRResultPage.swift
+//  iosAppPractice
+//
+//  Created by ta_fukase on 2019/03/12.
+//  Copyright © 2019 ta_fukase. All rights reserved.
+//
+
+import UIKit
+
+class QRResultPage: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
+
