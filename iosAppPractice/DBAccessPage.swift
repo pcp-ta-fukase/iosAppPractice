21aa0a3b37dc1a44b00b09bc9096a1d59f752d8f
ta_fukase <>
2019-03-12 14:56:30 +0900

    DBAccessPageにTableView/ TextField/ Buttonといった必要なUI部品を全て追加

--- iosAppPractice/DBAccessPage.swift
+++ iosAppPractice/DBAccessPage.swift
@@ -9,15 +9,62 @@
 
 import UIKit
 
-class DBAccessPage: UIViewController {
+class DBAccessPage: UIViewController, UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate {
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    @IBOutlet weak var nameTableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        nameTextField.delegate = self
+        nameTableView.delegate = self
     }
     
     @IBAction func onButtonBack(_ sender: Any) {
         
         self.dismiss(animated: true, completion: nil)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        textField.text = "CHANGE!!"
+            
+        //Keyboardを閉じる
+        textField.resignFirstResponder()
+        
+        return true
+    }
+    
+    //keyboard以外の画面を押すと、keyboardを閉じる処理
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        if (self.nameTextField.isFirstResponder) {
+            
+            self.nameTextField.text = "YOU TOUCHED OUTSIDE!"
+            self.nameTextField.resignFirstResponder()
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    // セルを返す
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // セルにテキストを出力する。
+        let cell = tableView.dequeueReusableCell(withIdentifier:  "cell", for:indexPath as IndexPath)
+        
+        // セルに表示する値（ラベルの文字など）を設定する
+        cell.textLabel!.text = "あいうえお"
+        
+        return cell
+    }
+    
+    // セルをタップした時に呼ばれる
+    func tableView(_ table: UITableView,didSelectRowAt indexPath: IndexPath) {
+        print("セルをタップしました")
     }
 }
